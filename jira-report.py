b93f90ed8791599407c757eb7e39ba658b1ca08c
Dustin Black <dblack@redhat.com>
2025-04-16 15:34:16 +0200

    working runner script and fixed grace days error in jira-report

--- jira-report.py
+++ jira-report.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-Copyright 2023 Dustin Black
+Copyright 2025 Dustin Black
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -21,18 +21,6 @@ Work inspired by:
  https://github.com/jtaleric/nudge/tree/5cc1be48a64646839bd2f5aa751ac7266da7b3c9
 
 Copyright 2021 Joe Talerico
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
 """
 
 import sys
@@ -381,7 +369,7 @@ if args.recipients and not args.local:
                             value, "%a %d %b %Y, %I:%M%p"
                         )
                         delta = datetime.now() - updated_datetime
-                        if delta.days >= args.update_grace_days:
+                        if delta.days >= int(args.update_grace_days):
                             html_report.append(
                                 f"<b>{key}</b>: <span style='color:red'>{value}</span><br>"
                             )
---
+++ runner.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python3
+
+"""
+Copyright 2025 Dustin Black
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import sys
+import subprocess
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+import yaml
+
+parser = ArgumentParser(
+    description="Runner from YAML for jira-report.py",
+    formatter_class=ArgumentDefaultsHelpFormatter,
+)
+
+# Passed-through arguments
+parser.add_argument(
+    "-S",
+    "--server",
+    type=str,
+    dest="jira_server",
+    required=True,
+    help="Jira server URL",
+)
+parser.add_argument(
+    "-T",
+    "--token",
+    type=str,
+    dest="jira_token",
+    required=True,
+    help="Jira authentication token",
+)
+parser.add_argument(
+    "-e",
+    "--email-server",
+    type=str,
+    dest="email_server",
+    required=True,
+    help="Email SMTP server URL (assumes SSL)",
+)
+# parser.add_argument(
+#     "-p",
+#     "--smtp-port",
+#     type=int,
+#     dest="smtp_port",
+#     required=True,
+#     default=465,
+#     help="Email SMTP server port number",
+# )
+parser.add_argument(
+    "-f",
+    "--email-from",
+    type=str,
+    dest="email_from",
+    required=True,
+    help="Email address to send from",
+)
+parser.add_argument(
+    "-u",
+    "--email-user",
+    type=str,
+    dest="email_user",
+    required=True,
+    help="Email user address if different than email from address",
+)
+parser.add_argument(
+    "-w",
+    "--email-password",
+    type=str,
+    dest="email_password",
+    required=True,
+    help="Email password (make sure you use an app password!)",
+)
+
+# Runner arguments
+parser.add_argument(
+    "-j",
+    "--job-id",
+    type=str,
+    dest="job_id",
+    required=True,
+    help="The job ID from the YAML input to run"
+)
+parser.add_argument(
+    "-i",
+    "--input",
+    type=str,
+    dest="input_path",
+    required=True,
+    help="The path to the YAML input file"
+)
+
+args = parser.parse_args()
+
+try:
+    with open (args.input_path, "r") as stream:
+        jobs = yaml.safe_load(stream)
+except:
+    print("Error reading input file!")
+    sys.exit(1)
+
+print(jobs)
+
+for job in jobs["jira_report_jobs"]:
+    if args.job_id in job["job_id"]:
+        myjob = job
+        break
+
+print(f"\n{myjob}")
+
+cmd = [
+        "./jira-report.py",
+        "-S",
+        args.jira_server,
+        "-T",
+        args.jira_token,
+        "-e",
+        args.email_server,
+        # "-p",
+        # args.smtp_port,
+        "-f",
+        args.email_from,
+        "-u",
+        args.email_user,
+        "-w",
+        args.email_password,
+        "-r",
+        ",".join(myjob["email"]["recipients"]),
+        "-s",
+        f"\"{myjob["email"]["subject"]}\"",
+        "-m",
+        f"\"{myjob["email"]["message"]}\"",
+        "-J",
+        f"\"{myjob["jql"]}\"",
+        #TODO make optional
+        "-x",
+        ",".join(myjob["exclude_comment_authors"]),
+        "-g",
+        str(myjob["update_grace_days"]),
+    ]
+
+print(f"\n{' '.join(cmd)}")
+
+try:
+    cmd_out = subprocess.check_output(
+        cmd,
+        stderr=subprocess.STDOUT,
+        text=True,
+    )
+    print(f"Job completed:\n{cmd_out}")
+except subprocess.CalledProcessError as err:
+    print(
+        f"{err.cmd[0]} failed with return code {err.returncode}:\n{err.output}"
+    )
+    sys.exit(1)
